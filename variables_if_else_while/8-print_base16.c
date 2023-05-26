6d19c26ead2369e6ed9776a033426ea77344f00d
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-05-26 09:56:28 -0700

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- count in hexa
+ *
+ * Return: Always 0, success
+ */
+int main(void)
+{
+	int hxd = 0;
+
+	while(hxd < 16)
+	{
+		if(hxd < 10)
+			putchar('0' + hxd);
+		else
+			putchar('a' + hxd - 10);
+
+		hxd++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
