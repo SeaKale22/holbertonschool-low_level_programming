9c85f6ed16271ff17165b81968f6d95b59279d70
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-05-26 09:04:44 -0700

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ *
+ * Return 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar('0' + n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
