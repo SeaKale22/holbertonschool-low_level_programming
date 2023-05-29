727d9c35f41217b8f805a0842486319d329ce30f
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-05-29 12:02:08 -0700

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * main- prints alphabet followed by newline
- * function print_alphabet- look at the code
+ * print_alphabet- look at the code
  * Return: Always 0
  */
 void print_alphabet(void)
