760a39f2d91fe8672ce947a92cd07b8423508e47
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-05-29 11:59:37 -0700

    1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * main- prints alphabet followed by newline
+ * function print_alphabet- look at the code
  * Return: Always 0
  */
 void print_alphabet(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
