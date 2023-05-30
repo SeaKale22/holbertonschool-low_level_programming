c32940930ef4e8a4fe1714b8d898430ac13a377a
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-05-30 12:37:10 -0700

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 	int hour;
 	int min;
 
-	for (hour = 0; hour < 24; hour ++)
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (min = 0; min < 60; min ++)
+		for (min = 0; min < 60; min++)
 		{
 			_putchar('0' + hour / 10);
 			_putchar('0' + hour % 10);
