89fcba9539d61e9189f756936ab3a8756bc4fee7
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-02 10:25:56 -0700

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,14 +6,14 @@
 void more_numbers(void)
 {
 	int r;
-	
+
 	for (r = 0; r < 10; r++)
 	{
 		int i;
-		
+	
 		for (i = 0; i <= 14; i++)
 		{
-			if ( i > 9)
+			if (i > 9)
 				_putchar(i / 10 + '0');
 
 			_putchar(i % 10 + '0');
