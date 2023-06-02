1efaaed3aadca6eb92a5e5130835b6ec9a414e18
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-02 09:14:14 -0700

    2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Multiplies to numbers
+ * @a: The number to be multiplied by b
+ * @b: The number that multiplies a
+ * @p: The product of a multiplied by b
+ * Return: The product of a multiplied by b
+ */
+int mul(int a, int b)
+{
+	int p;
+	p = a * b;
+	return (p);
+}
