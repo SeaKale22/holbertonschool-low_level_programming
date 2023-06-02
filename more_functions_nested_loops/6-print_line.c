f939c4429421aeef29e02308dc6d0a81c9c0bb0b
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-02 11:38:37 -0700

    6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * print_line - prints _ n number of times
- * @n: The number tomes _ should be printed
+ * print_line - Prints '_' n number of times
+ * @n: The number times '_' should be printed
+ *
  * Return: Void
  */
 void print_line(int n)
 {
 	int l;
+
 	for (l = 0; l < n; l++)
 	{
 		_putchar('_');
