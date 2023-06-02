548f0cf77756660b2cd43e91e86a577567778a0e
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-02 09:19:25 -0700

    2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,12 +2,14 @@
 /**
  * mul - Multiplies to numbers
  * @a: The number to be multiplied by b
- * @b: The number that multiplies a 
+ * @b: The number that multiplies a
+ *
  * Return: The product of a multiplied by b
  */
 int mul(int a, int b)
 {
 	int p;
+
 	p = a * b;
 	return (p);
 }
