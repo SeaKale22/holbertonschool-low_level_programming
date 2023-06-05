97e0e208095e374e76202424fe65d293d103d7d1
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-05 12:41:38 -0700

    6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2- prints every other character of a string, followed by a newline
+ * @str - the string to be printed
+ */
+void puts2(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		if (index % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
+		index++;
+	}
+	_putchar('\n');
+}
