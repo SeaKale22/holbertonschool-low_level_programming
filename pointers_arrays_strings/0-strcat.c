6ae6841cf647db0a27d591527af6794183d91c85
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-07 10:56:45 -0700

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat- concatenares two strings
+ * @dest: first string to be cat
+ * @src: second string to be cat
+ * Retrun: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int destlngth = 0;
+	int srclngth = 0;
+	int i;
+	
+	while (dest[destlngth] != '\0')
+	{
+		destlngth++;
+	}
+	while (src[srclngth] != '\0')
+	{
+		srclngth++;
+	}
+	for (i = 0; i < srclngth; i++)
+	{
+		dest[destlngth + i] = src[i];
+	}
+	dest[destlngth + srclngth] = '\0';
+
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,16 +1,7 @@
-#ifndef main_h
-#define main_h
+#ifndef more_pointers_arrays_strings_h
+#define more_pointers_arrays_strings_h
 
 int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 
 #endif
