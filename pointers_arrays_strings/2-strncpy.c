ecca9ef062108d1a9a9e8f7b091fda21ce64fdf0
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-08 10:47:03 -0700

    2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy- copies a string
+ * @dest: Destination of copied string
+ * @src: Sorce string to be copied
+ * @n: max number of characters to be copied
+ * Return: Pointer to dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int sl = 0;
+	int i;
+
+	while (src[sl] != '\0' && sl < n)
+	{
+		sl++;
+	}
+
+	for (i = 0; i < sl; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[sl + 1] = '\0';
+	
+	return (dest);
+}
