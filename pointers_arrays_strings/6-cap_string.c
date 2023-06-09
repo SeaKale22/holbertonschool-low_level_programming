2e06ee55e620ffdb7ebba9b3c3cd0d3c0dce6be1
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-09 13:12:43 -0700

    7

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 	    int i;
 	    int cap_next = 1;
 	    int word_start = 1;
-	    
+ 
 	    for (i = 0; str[i] != '\0'; i++)
 	    {
 		    if (str[i] >= 'a' && str[i] <= 'z')
