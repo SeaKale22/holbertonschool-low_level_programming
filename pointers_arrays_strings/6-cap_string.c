73a60e1c8c95d416fb66e9a25ab8eb51aa045803
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-09 11:21:03 -0700

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string- capitalizes all words of a string
+ * @str: String to be modified
+ * Return: The modified string
+ */
+char *cap_string(char *str)
+{
+	int capnxt = 1;
+	int i;
+
+	for ( i = 0; str[i] != '\0'; i++)
+	{
+		if (capnxt && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] -= 'a' - 'A';
+			capnxt = 0;
+		}
+		else if (!capnxt && (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			str[i] += 'a' - 'A';
+		}
+
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || 
+				str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+				capnxt = 1;
+		}
+	}
+	return (str);
+}
