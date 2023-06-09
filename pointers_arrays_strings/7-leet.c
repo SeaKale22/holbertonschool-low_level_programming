84177db2acaa36c9ed81770aec24841031cfe5a9
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-09 13:35:29 -0700

    7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,28 +7,19 @@
 char *leet(char *str)
 {
 	int i;
+	char repl[] = "aAeEoOtTlL";
+	char leet[] = "43071";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		int j;
+		for (j = 0; repl[j] != '\0'; j++)
 		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
+			if (str[i] == repl[j])
+			{
+				str[i] = leet[j];
+				break;
+			}
 		}
 	}
 	return (str);
