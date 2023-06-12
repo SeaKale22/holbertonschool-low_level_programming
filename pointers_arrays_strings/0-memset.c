00a97aab7796291b2670125cc644431c292576e1
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-12 08:39:49 -0700

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset- fills memory with a constant byte\
+ * _memset - fills memory with a constant byte\
  * @s : pointer to memory area to be filled
  * @b : constant byte
  * @n : memory to be filled
