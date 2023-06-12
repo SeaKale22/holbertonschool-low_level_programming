4c779c481587066455a75714371359f772441739
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-12 14:19:24 -0700

    5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strstr- finds a substring
+ * @haystack: The string to be searched
+ * @needle: Substring to be located
+ *
+ * Return: pointer to beging of substring or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int start;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			start = i;
+
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					break;
+				}
+			}
+			if (needle[j] == '\0')
+			{
+				return (&haystack[start]);
+			}
+		}
+	}
+	return ('\0');
+}
