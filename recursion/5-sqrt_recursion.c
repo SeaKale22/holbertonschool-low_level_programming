6582c16bc594c451e5d92f8d0de8010790315162
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-14 14:25:03 -0700

    5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _sqrt_recursion- find the square root of a number
+ * @n: the number in question
+ *
+ * Return: the natral square root
+ */
+int sqrtfinder(int n, int low, int high);
+
+int _sqrt_recursion(int n)
+{
+	return (sqrtfinder(n, 0, n));
+}
+
+int sqrtfinder(int n, int low, int high)
+{
+	int mid;
+
+	if (low > high)
+	{
+		return (-1);
+	}
+	mid = low + (high - low) / 2;
+
+	if (mid * mid == n)
+	{
+		return (mid);
+	}
+	else if (mid * mid < n)
+	{
+		return (sqrtfinder(n, mid + 1, high));
+	}
+	else
+	{
+		return (sqrtfinder(n, low, mid -1));
+	}
+}
---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * is_prime_number- checks if number is prime
+ * @n: the number to be checked
+ * 
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+
