b6ee74eb5fa5be84651d94f1a86a5236ce9434c0
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-15 08:06:23 -0700

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,9 +2,31 @@
 /**
  * is_prime_number- checks if number is prime
  * @n: the number to be checked
+ *
+ * is_divisable- checks if n is divisable by a number other than 1
+ * @di: the number to check n against
  * 
  * Return: 1 if prime, 0 otherwise
  */
+int is_divisable(int i, int di);
+
 int is_prime_number(int n)
 {
-
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (is_divisable(n, n / 2));
+}
+int is_divisable(int n, int di)
+{
+	if (di == 1)
+	{
+		return (1);
+	}
+	if (n % di == 0)
+	{
+		return (0);
+	}
+	return (is_divisable(n, di - 1));
+}
