4b6415dc70b3299e0f06479c898e34082d64c5c2
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-15 16:54:41 -0700

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- pritns num of args
+ * @argc: count of args
+ * @argv: array of pointers to arg strings
+ *
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n",argc - 1);
+	return (0);
+	(void)argv;
+
+}
