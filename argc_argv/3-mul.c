2723f67b7a5da9fad2d5e595b427a04a6e4d1488
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-16 10:09:29 -0700

    3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main- multiplies two numbers
+ * @ardc: count of arguments
+ * @argv: vectors of arguments, should be two ints
+ *
+ * Return: 0 if sucssess, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	int sum;
+
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	sum = a * b;
+	printf("%d\n", sum);
+	return (0);
+	
+}
