f9dad968c70605075da8f10d8ffa49c2c01b91a5
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-23 11:08:56 -0700

    0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked- allocated memory
+ * @b: bytes to be assigned
+ * 
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *mem;
+
+	mem = malloc(b);
+	if ( mem == NULL)
+	{
+		exit(98);
+	}
+	return(mem);
+}
---
+++ more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef moremalloc
+#define moremalloc
+
+void *malloc_checked(unsigned int b);
+
+#endif
