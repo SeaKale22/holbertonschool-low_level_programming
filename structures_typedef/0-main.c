e3a0188e0a21896d89e8fd2f6a8873a1e974b3a8
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-28 12:14:40 -0700

    0

--- structures_typedef/0-main.c
+++ structures_typedef/0-main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include "dog.h"
-;int main(void)
-{
-	    struct dog my_dog;
 
-	        my_dog.name = "Poppy";
-		    my_dog.age = 3.5;
-		        my_dog.owner = "Bob";
-			    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-			        return (0);
+/**
+ *  * main - check the code .
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+		struct dog my_dog;
+
+			my_dog.name = "Ghost";
+				my_dog.age = 4.75;
+					my_dog.owner = "Jon Snow";
+						printf("My name is %s, I am %.2f, and my owner is %s\n",
+										my_dog.name, my_dog.age, my_dog.owner);
+							return (0);
 }
