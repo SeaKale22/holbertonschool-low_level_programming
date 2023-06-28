a5907453f9c022da0709dfa794555afdca80fe25
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-28 13:09:38 -0700

    2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog- prints a dog structure
+ * @d: dog structure to be printed
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+	if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+	printf("Name: %s\n", d->name);
+	if (d->age == 0.0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else 
+	{
+		printf("Age: %f\n", d->age);
+	}
+	printf("Owner: %s\n",d->owner);
+}
