f1633bbbd3432787a0ee14ea11015b165b5c7a41
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-29 16:30:11 -0700

    3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main- performs simple ops
+ * @argc: arg count
+ * @argv: arg vectors
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit (99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	result = operation(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
+}
