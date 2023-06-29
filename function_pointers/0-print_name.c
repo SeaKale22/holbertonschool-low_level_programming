99cbd9d683d35673e201904ce39405845c744bca
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-06-29 10:10:35 -0700

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name- prints a name
  * @name: name to be printed
@@ -6,5 +7,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f != NULL)
+	{
 	f(name);
+	}
 }
