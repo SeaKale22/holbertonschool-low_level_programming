b61a1e5a6a93999f28e2996e5c9146570e29ab7f
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-03 14:54:54 -0700

    1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		num = va_arg(nums, int);
 		printf("%d", num);
-		if (i < (n - 1))
+		if (separator != NULL && i < (n - 1))
 		{
 			printf("%s", separator);
 		}
