130f2ed7f81fd0ec5b5a0cb852959e43acb5be4a
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-13 18:41:35 -0700

    1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,3 +3,17 @@
  * list_len- finds number of elements in a linked list
  * @h: the head of the linked list
  *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
