1f7b11e216536da7b383bb781713e16092574b5f
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-13 18:18:53 -0700

    0

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,5 @@
+#include "lists.h"
+/**
+ * list_len- finds number of elements in a linked list
+ * @h: the head of the linked list
+ *
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -4,10 +4,6 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-int _putchar(char c);
-size_t print_list(const list_t *h);
-
-
 /**
  *  * struct list_s - singly linked list
  *   * @str: string - (malloc'ed string)
@@ -19,8 +15,11 @@ size_t print_list(const list_t *h);
 typedef struct list_s
 {
 	    char *str;
-	    unsigned int len;
-	    struct list_s *next;
+	        unsigned int len;
+		    struct list_s *next;
 } list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
