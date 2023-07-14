dc55a63535935931f776b27137c4a28764dcb530
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-14 07:12:55 -0700

    3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -19,6 +19,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
@@ -38,6 +42,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		list_t *current = *head;
+
 		while (current->next != NULL)
 		{
 			current = current->next;
