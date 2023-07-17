9d2253a3036a01fd9b12a194e422c0c35d60340b
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-17 16:58:47 -0700

    5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -5,14 +5,16 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
-	listint_t *next;
-	*head = NULL;
-
-	while (current != NULL)
+	if (head != NULL)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		listint_t *current = *head;
+		listint_t *next;
+		*head = NULL;
+		while (current != NULL)
+		{
+			next = current->next;
+			free(current);
+			current = next;
+		}
 	}
 }
