5a63b8e77a456fa658d10f3ed001167ff7e32145
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-17 16:43:13 -0700

    4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint- frees a list
+ * @head: head of list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
