890a7a9c27a5c65dd9b4f62a500e1e6e38d65070
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-21 12:58:30 -0700

    4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (ht == NULL)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 	if (node == NULL)
