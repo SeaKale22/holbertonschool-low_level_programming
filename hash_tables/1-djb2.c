681537a98318fd37af8553cf1e180d3abb5054a5
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-21 09:28:19 -0700

    1

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,3 +1,9 @@
+/**
+ * hash_djb2- creates a hash code
+ * @str: string to make hash code from
+ *
+ * Return: The hash code
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
