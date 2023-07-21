059471c3896a4d701a3681363a543e04f845c1f6
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-07-21 12:54:55 -0700

    4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get- finds value associated with a key
+ * @ht: hash table to searh
+ * @key: the key
+ *
+ * Return:value assocaited with element, or NULL of key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char *value;
+	unsigned long int index;
+	hash_node_t *node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (node == NULL)
+		return (NULL);
+	value = node->value;
+	return (value);
+}
