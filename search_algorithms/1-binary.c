88c4b63c5bb6de4c89c386ca9ec1eeafdfc8cb51
SeaKale22 <kaleb.tahji.k@gmail.com>
2023-08-16 08:48:14 -0700

    added binary search

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+/**
+ * binary_search- searches for a value in an array using binary search
+ * @array: pointer to first element of array
+ * @size: size of array
+ * @value: Value being searched for
+ *
+ * Return: index where value is located
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0;
+	int right = size - 1;
+	int mid;
+	int i;
+
+	if (!array || size == 0)
+		return -1;
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+		}
+		printf("\n");
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
